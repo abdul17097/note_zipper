a54da5bafd9e47b618ed2d3871fede0037507c2f
abdul17097 <abdul17097@gmail.com>
2023-08-23 09:41:03 -0700

    initial

--- frontendnotezipper/src/component/Screen/MyNotes/Note.js
+++ frontendnotezipper/src/component/Screen/MyNotes/Note.js
@@ -37,7 +37,7 @@ export const Note = ({note}) => {
       })
     }
   return (
-    <div className={`flex box-border flex-col  ${toggle?'':'h-12'}`}>
+    <div className={`flex box-border flex-col ${toggle?'':'h-12'}`}>
         <div className={`flex justify-between  px-4 py-1 border items-center ${toggle? 'rounded-t-md': 'rounded-md'}`} onClick={()=> setToggle(!toggle)}>
         <h2 className='font-bold'>{title}</h2>
         <div className='flex gap-5  '>
@@ -45,8 +45,8 @@ export const Note = ({note}) => {
             <div  className='border h-10 w-16 flex justify-center items-center cursor-pointer' onClick={deleteNote}>Delete</div>
         </div>
         </div>
-        <div className={`flex p-2 border rounded-b-md border-t-0 ${toggle?'visible h-32':'invisible h-none'}`}>
-           <div dangerouslySetInnerHTML={{ __html: content }} />
+        <div className={`flex p-2 border  rounded-b-md border-t-0 ${toggle?'visible h-52 mb-3 overflow-auto':'invisible h-none'}`}>
+           <div  dangerouslySetInnerHTML={{ __html: content }} />
         </div>
     </div>
   )
